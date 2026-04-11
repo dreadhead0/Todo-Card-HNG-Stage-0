57a0d97d58ad4e90c948a6614eb232626d45b88d
Precious Odion <preciousodion2211@gmail.com>
2026-04-11 18:36:44 +0100

    fixed the date and time remaining intervals and changed some of the styling.

--- script.js
+++ script.js
@@ -1,70 +1,101 @@
-const checkbox = document.querySelector('[data-testid="test-todo-complete-toggle"]');
-const title = document.querySelector('[data-testid="test-todo-title"]');
-const statusText = document.querySelector('[data-testid="test-todo-status"]');
-const timeRemaining = document.querySelector('[data-testid="test-todo-time-remaining"]');
-const editBtn = document.querySelector('[data-testid="test-todo-edit-button"]');
-const deleteBtn = document.querySelector('[data-testid="test-todo-delete-button"]');
+    const checkbox = document.querySelector('[data-testid="test-todo-complete-toggle"]');
+    const title = document.querySelector('[data-testid="test-todo-title"]');
+    const statusText = document.querySelector('[data-testid="test-todo-status"]');
+    const timeRemaining = document.querySelector('[data-testid="test-todo-time-remaining"]');
+    const editBtn = document.querySelector('[data-testid="test-todo-edit-button"]');
+    const deleteBtn = document.querySelector('[data-testid="test-todo-delete-button"]');
 
-// for null safety to prevent silent failures
-if (!checkbox || !title || !statusText || !timeRemaining) {
-  console.error("Missing DOM elements");
-}
+    // for null safety to prevent silent failures
+    if (!checkbox || !title || !statusText || !timeRemaining || !editBtn || !deleteBtn) {
+    console.error("Missing DOM elements");
+    }
 
-// function to update time remaining
-function updateTimeRemaining() {
-    const dueDate = new Date("2026-04-16T18:00:00");
+    // function to update time remaining
+    function updateTimeRemaining() {
+    // new Date(Year, month, day, Hour, Minute, Second);
+    const dueDate = new Date(2026, 3, 16, 18, 0, 0);
+    // April = 3 because of Javascript 0-based indexing
     const now = new Date();
 
     const diffMs = dueDate - now;
 
-    const diffHours = Math.floor(diffMs / (1000 * 60 * 60));
-    const diffDays = Math.floor(diffHours /24);
+    const diffMinutes = Math.floor(diffMs / (1000 * 60));
+    const diffHours = Math.floor(diffMinutes / 60);
+    const diffDays = Math.floor(diffHours / 24);
 
     let message = "";
 
+    // Handles overdue case
     if (diffMs <= 0) {
-        message = diffHours < 0 
-            ? `Overdue by ${Math.abs(diffHours)} hours`
-            : "Due now!";   
+        const overdueMinutes = Math.abs(diffMinutes);
+        const overdueHours = Math.floor(overdueMinutes / 60);
+        const overdueDays = Math.floor(overdueHours / 24);
+
+        const remHours = overdueHours % 24;
+        const remMinutes = overdueMinutes % 60;
+
+        if (overdueDays > 0) {
+            message = `Overdue by ${overdueDays} days ${remHours} hours`;
+        } 
+        else if (overdueHours > 0) {
+            message = `Overdue by ${overdueHours} hours ${remMinutes} minutes`;
+        } 
+        else {
+            message = `Overdue by ${overdueMinutes} minutes`;
+        }
     }
-    else if (diffDays === 0) {
-            message = "Due today!";
+
+    // less than one hour remaining
+    else if (diffMinutes < 60) {
+        const m = diffMinutes;
+        message = `Due in ${m} minute${m === 1 ? "" : "s"}`;
+    }
+
+    // less than 24 hours
+    else if (diffHours < 24) {
+        const h = diffHours;
+        const m = diffMinutes % 60;
+
+        message = `Due in ${h} hour${h === 1 ? "" : "s"} ${m} minute${m === 1 ? "" : "s"}`;
+    }
+
+    // multi-days
+    else if (diffDays === 1) {
+        message = "Due tomorrow";
     } 
-    else if (diffDays === 1) { 
-            message = "Due tomorrow";
-    }
     else {
-        message = `Due in ${diffDays} days`;
+        const remHours = diffHours % 24;
+        message = `Due in ${diffDays} days ${remHours} hours`;
     }
 
     timeRemaining.textContent = message;
 }
 
-// call the function
-updateTimeRemaining();
+    // call the function
+    updateTimeRemaining();
 
-// time interval
-setInterval(updateTimeRemaining, 60000);
+    // time interval
+    setInterval(updateTimeRemaining, 1000);
 
-checkbox.addEventListener("change", () => {
-    title.classList.toggle("done");
+    checkbox.addEventListener("change", () => {
+        title.classList.toggle("done");
 
-    if (checkbox.checked) {
-        statusText.textContent = "Done";
-        statusText.classList.add("done-status");
-    } else {
-        statusText.textContent = "Pending";
-        statusText.classList.remove("done-status");
-    }
-});
+        if (checkbox.checked) {
+            statusText.textContent = "Done";
+            statusText.classList.add("done-status");
+        } else {
+            statusText.textContent = "Pending";
+            statusText.classList.remove("done-status");
+        }
+    });
 
-editBtn.addEventListener("click", () => {
-    console.log("edit clicked");
-});
+    editBtn.addEventListener("click", () => {
+        console.log("edit clicked");
+    });
 
-deleteBtn.addEventListener("click", () => {
-    const confirmDelete = confirm("Are you sure?");
-    if (confirmDelete) {
-        alert('Delete clicked');
-    }
-});
+    deleteBtn.addEventListener("click", () => {
+        const confirmDelete = confirm("Are you sure?");
+        if (confirmDelete) {
+            alert('Delete clicked');
+        }
+    });
